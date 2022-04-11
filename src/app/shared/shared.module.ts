0e3407dc5f2f5ce0b609237ee9c90b59c488c3ca
Jhon Moreno <101996083+jsmorenor@users.noreply.github.com>
2022-04-11 18:39:20 -0500

    Created a basic header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
-
+import { HeaderComponent } from '@shared/components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class SharedModule { }
