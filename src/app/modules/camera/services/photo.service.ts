392fb876e9a3d05b103714506df0f64688372c04
Jhon Moreno <101996083+jsmorenor@users.noreply.github.com>
2022-04-13 23:55:01 -0500

    The photos are saved in the file system.

--- src/app/modules/camera/services/photo.service.ts
+++ src/app/modules/camera/services/photo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { UserPhoto } from '@core/models/user-photo';
+import { Directory, Filesystem } from '@capacitor/filesystem';
 
 @Injectable({
   providedIn: 'root',
@@ -24,9 +25,39 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100,
     });
-    this.photos.unshift({
-      filePath: '...',
-      webviewPath: capturedPhoto.webPath,
-    });
+
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
   }
+
+  private async savePicture(photo: Photo) {
+    const data = await this.readAsBase64(photo);
+
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      directory: Directory.Data,
+      path: fileName,
+      data,
+    });
+
+    return {
+      filePath: fileName,
+      webviewPath: photo.webPath,
+    };
+  }
+
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath as string);
+    const blob = await response.blob();
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
